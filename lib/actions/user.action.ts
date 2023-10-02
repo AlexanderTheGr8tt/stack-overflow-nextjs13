10c8c0199192b1be3dc3a3079d24524937c0a4b8
AlexanderTheGr8tt <aleksandermalecki@hotmail.com>
2023-10-02 12:01:23 +0200

    s

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -66,6 +66,7 @@ export async function deleteUser(params: DeleteUserParams) {
     }
 
     // get user question ids
+    ///
     const userQuestionIds = await Question.find({ author: user._id }).distinct(
       "_id"
     );
