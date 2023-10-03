5d6556c0cfcb11c893be4c22320804867a99eb7a
AlexanderTheGr8tt <aleksandermalecki@hotmail.com>
2023-10-03 16:53:12 +0200

    props interface

--- app/(root)/question/[id]/page.tsx
+++ app/(root)/question/[id]/page.tsx
@@ -3,6 +3,7 @@ import AllAnswers from "@/components/shared/AllAnswers";
 import Metric from "@/components/shared/Metric";
 import ParseHTML from "@/components/shared/ParseHTML";
 import RenderTag from "@/components/shared/RenderTag";
+import Votes from "@/components/shared/Votes";
 import { getQuestionById } from "@/lib/actions/question.action";
 import { getUserById } from "@/lib/actions/user.action";
 import { formatAndDivideNumber, getTimestamp } from "@/lib/utils";
@@ -12,7 +13,12 @@ import Link from "next/link";
 
 import React from "react";
 
-const page = async ({ params, searchParams }) => {
+interface Props {
+  params: any;
+  searchParams: any;
+}
+
+const page = async ({ params, searchParams }: Props) => {
   const result = await getQuestionById({ questionId: params.id });
   const { userId: clerkId } = auth();
 
@@ -41,7 +47,9 @@ const page = async ({ params, searchParams }) => {
             </p>
           </Link>
 
-          <div className="flex justify-end">voting</div>
+          <div className="flex justify-end">
+            <Votes />
+          </div>
         </div>
 
         <h2 className="h2-semibold text-dark200_light900 mt-3.5 w-full text-left">
