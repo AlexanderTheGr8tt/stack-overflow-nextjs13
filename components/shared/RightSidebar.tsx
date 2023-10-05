974a7eee497fd60889e452c15d6e1d8777c9267c
AlexanderTheGr8tt <aleksandermalecki@hotmail.com>
2023-10-05 21:42:04 +0200

    added showing topQuestions/Tags

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -2,48 +2,13 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import RenderTag from "./RenderTag";
+import { getHotQuestions } from "@/lib/actions/question.action";
+import { getTopPopularTags } from "@/lib/actions/tag.actions";
 
-const RightSidebar = () => {
-  const hotQuestions = [
-    {
-      _id: "1",
-      title:
-        "Best practices for data fetching in a Next.js application with Server-Side Rendering (SSR)? Chevron right icon",
-    },
-    {
-      _id: "2",
-      title:
-        "How do I use express as a custom server in NextJS? Chevron right icon",
-    },
-    { _id: "3", title: "Can I get the course for free? Chevron right icon" },
-    {
-      _id: "4",
-      title: "Redux Toolkit Not Updating State as Expected Chevron right icon",
-    },
-  ];
+const RightSidebar = async () => {
+  const hotQuestions = await getHotQuestions();
+  const popularTags = await getTopPopularTags();
 
-  const popularTags = [
-    {
-      _id: "1",
-      name: "REACT.js",
-      totalQuestions: 7,
-    },
-    {
-      _id: "2",
-      name: "NEXT js",
-      totalQuestions: 45,
-    },
-    {
-      _id: "3",
-      name: "JavaScript",
-      totalQuestions: 3,
-    },
-    {
-      _id: "4",
-      name: "MongoDB",
-      totalQuestions: 1,
-    },
-  ];
   return (
     <section className="custom-scrollbar background-light900_dark200 light-border sticky right-0 top-0 flex h-screen w-[350px] flex-col gap-6 overflow-y-auto border-l p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden">
       <div>
@@ -52,7 +17,7 @@ const RightSidebar = () => {
           {hotQuestions.map((question) => (
             <Link
               key={question._id}
-              href={`/questions/${question._id}`}
+              href={`/question/${question._id}`}
               className="flex cursor-pointer items-center justify-between gap-7"
             >
               <p className="body-medium text-dark500_light700">
@@ -78,7 +43,7 @@ const RightSidebar = () => {
               key={tag._id}
               _id={tag._id}
               name={tag.name}
-              totalQuestions={tag.totalQuestions}
+              totalQuestions={tag.numberOfQuestions}
               showCount
             />
           ))}
